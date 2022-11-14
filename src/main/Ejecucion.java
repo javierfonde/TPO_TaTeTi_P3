d992f7acaa5b626c393dbd658d54ccc275c648e3
javierfonde <74280718+javierfonde@users.noreply.github.com>
2022-11-13 21:09:55 -0300

    Cambios en solicitud de posicion

--- src/main/Ejecucion.java
+++ src/main/Ejecucion.java
@@ -39,18 +39,20 @@ public class Ejecucion {
 		
 		do {
 			
-			int fila = 3;
-			do{
-				System.out.println("Ingrese el numero de fila (0, 1 o 2)");
-				fila = scan.nextInt();
-			}while(fila < 0 && fila > 2);
+			System.out.println("Ingrese el numero de fila (1,2 o 3)");
+			int fila = scan.nextInt()-1;
+			while(fila < 0 || fila > 2) {
+				System.out.println("ERROR. Ingrese el numero de fila VALIDO (1,2 o 3)");
+				fila = scan.nextInt()-1;
+			}
 			
+			System.out.println("Ingrese el numero de columna (1,2 o 3)");
+			int columna = scan.nextInt()-1;
+			while(columna < 0 || columna > 2) {
+				System.out.println("ERROR.Ingrese el numero de columna VALIDO (1,2 o 3)");
+				columna = scan.nextInt()-1;
+			}
 			
-			int columna = 3;
-			do {
-				System.out.println("Ingrese el numero de columna (0, 1 o 2)");
-				columna = scan.nextInt();
-			}while(columna < 0 && columna > 2);
 			
 			esCorrecto = tateti.jugar(fila, columna);
 			
--- src/objetos/Tateti.java
+++ src/objetos/Tateti.java
@@ -99,14 +99,14 @@ public class Tateti implements ITateti{
 	}
 	
 	public void mostrarTablero() {
-		System.out.println("-------------");
+		System.out.println("----------");
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				System.out.print(" " + tablero[i][j] + " ");
 				if(j==2) System.out.println();
 			}
 		}
-		System.out.println("-------------");
+		System.out.println("----------");
 	}
 	
 	public boolean esTurnoPersona() {
